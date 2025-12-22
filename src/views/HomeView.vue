3c4817e8306edf647f9fd09d0d69d65ebc8016fa
Sylvain Garnot <sylvain.garnot@gmail.com>
2025-12-22 13:33:58 +0100

    clean template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    accueil
   </main>
 </template>
+
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
